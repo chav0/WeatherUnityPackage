ba84081bd7cd4758292fc234287d6fed085d6003
Alina Chupakhina <chav0@mail.ru>
2024-01-03 22:09:54 +0100

    Fix

--- Runtime/LocationProviders/AndroidLocationProvider.cs
+++ Runtime/LocationProviders/AndroidLocationProvider.cs
@@ -3,7 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace WeatherServices.LocationProviders
+namespace WeatherServices
 {
 #if UNITY_ANDROID
         public class AndroidLocationProvider : BaseRuntimeLocationProvider
